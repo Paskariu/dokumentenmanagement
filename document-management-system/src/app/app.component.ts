d9dad7b56b51cc8e6d761261d7268d00300b231c
paskariu <rosine.95@gmx.de>
2022-03-28 14:49:42 +0200

    search only shows up on home page

--- document-management-system/src/app/app.component.ts
+++ document-management-system/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { NavigationEnd, Router } from '@angular/router';
 import { DisplayFileComponent } from './search-page/display-file/display-file.component';
 
 @Component({
@@ -15,7 +16,14 @@ export class AppComponent {
     "name": "word.docx",
     "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   }];
-  constructor(private http: HttpClient,public dialog: MatDialog) { }
+  currentUrl:any = "";
+  constructor(private http: HttpClient,public dialog: MatDialog, private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {        
+        this.currentUrl = event.url;
+      };
+ });
+  }
 
   onClick(event:any){
     this.http.get('http://localhost:3000/search/' + '?searchField='+ encodeURIComponent( JSON.stringify(this.msg)))
